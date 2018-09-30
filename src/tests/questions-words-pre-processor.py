fbdc80adceaa6728f8eda8519d3c8b85f2b39687
Ronald <ronald.drp11@gmail.com>
2018-09-30 18:15:13 -0300

    updated test bash script and renamed questions-words pre-processor python script

--- src/tests/questions-words-pre-processor.py
+++ src/tests/questions-words-pre-processor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-with open('../word2vec/questions-words.txt', 'r') as questionWords, open('../word2vec/question-words-lower-answer.txt', 'w') as output, open('../word2vec/question-words-lower-input.txt', 'w') as inputFile:
+with open('../word2vec/questions-words.txt', 'r') as questionWords, open('../word2vec/input/questions-words-lower-answer.txt', 'w') as output, open('../word2vec/input/questions-words-lower-input.txt', 'w') as inputFile:
     for line in questionWords:
         if line[0] != ':':
             line = line.lower()
--- src/tests/test-execution.sh
+++ src/tests/test-execution.sh
@@ -13,11 +13,9 @@ fi
 
 echo 'Splitting corpus into different percentages...'
 
-if [ ! -e input/text8-* ]; then
-    cd ../tests
-    python3 corpus-splitter.py
-    cd ../word2vec
-fi
+cd ../tests
+python3 corpus-splitter.py
+cd ../word2vec
 
 mkdir -p trained-models/
 mkdir -p output/
@@ -89,13 +87,49 @@ if [ ! -e trained-models/skip8.bin ]; then
     ./word2vec -train input/text8-80 -output trained-models/skip8.bin -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
 fi
 
-
 echo 'Finished training models'
+
+echo 'Executing question-words pre-processor...'
+
+cd ../tests
+python3 questions-words-pre-processor.py
+cd ../word2vec
+
+echo 'Finished question-words pre-processor'
 
 echo 'Executing word-analogy tests...'
 
-#./word-analogy trained-models/cbow1.bin < questions-words.txt > output/cbow1.txt
-#./word-analogy trained-models/skip1.bin < questions-words.txt > output/skip1.txt
+./word-analogy trained-models/cbow1.bin < input/questions-words-lower-input.txt > output/cbow1.txt
+
+./word-analogy trained-models/cbow2.bin < input/questions-words-lower-input.txt > output/cbow2.txt
+
+./word-analogy trained-models/cbow3.bin < input/questions-words-lower-input.txt > output/cbow3.txt
+
+./word-analogy trained-models/cbow4.bin < input/questions-words-lower-input.txt > output/cbow4.txt
+
+./word-analogy trained-models/cbow5.bin < input/questions-words-lower-input.txt > output/cbow5.txt
+
+./word-analogy trained-models/cbow6.bin < input/questions-words-lower-input.txt > output/cbow6.txt
+
+./word-analogy trained-models/cbow7.bin < input/questions-words-lower-input.txt > output/cbow7.txt
+
+./word-analogy trained-models/cbow8.bin < input/questions-words-lower-input.txt > output/cbow8.txt
+
+./word-analogy trained-models/skip1.bin < input/questions-words-lower-input.txt > output/skip1.txt
+
+./word-analogy trained-models/skip2.bin < input/questions-words-lower-input.txt > output/skip2.txt
+
+./word-analogy trained-models/skip3.bin < input/questions-words-lower-input.txt > output/skip3.txt
+
+./word-analogy trained-models/skip4.bin < input/questions-words-lower-input.txt > output/skip4.txt
+
+./word-analogy trained-models/skip5.bin < input/questions-words-lower-input.txt > output/skip5.txt
+
+./word-analogy trained-models/skip6.bin < input/questions-words-lower-input.txt > output/skip6.txt
+
+./word-analogy trained-models/skip7.bin < input/questions-words-lower-input.txt > output/skip7.txt
+
+./word-analogy trained-models/skip8.bin < input/questions-words-lower-input.txt > output/skip8.txt
 
 echo 'Finished word-analogy tests'
 
